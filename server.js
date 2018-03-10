3c5bd58adadbfb6b99b6c7cad678cd5e4251757f
ahmedakhtar11 <ahmedakhtar11@yahoo.com>
2018-03-10 16:43:34 -0600

    final

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
-// Dependencies
-// =============================================================
+// Required
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
-// Sets up the Express App
-// =============================================================
+// Express
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
+// Body Parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+// Routes
 var htmlRoutes = require('./app/routing/htmlRoutes.js');
 var apiRoutes = require('./app/routing/apiRoutes.js');
 app.use('/', htmlRoutes);
 app.use('/', apiRoutes);
-// Starts the server to begin listening
-// =============================================================
+
+// Listening
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
